178759c207eb201b9b41d1b224dbf4a941b28c47
Brendan Cheng <brendancheng333@gmail.com>
2019-11-14 21:40:57 -0800

    added logic to authenticate logins.

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,6 +18,48 @@ authRouter.get('/', auth, async (req, res) => {
 // @route   POST api/auth
 // @desc    Protected route to log a user in.
 // @access  Public
-authRouter.post('/', auth, (req, res) => {});
+authRouter.post(
+  '/',
+  [
+    check('email', 'Please enter a valid email.').isEmail(),
+    check('password', 'Password is required.').exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      //check if user exists already.
+      let foundUser = await User.findOne({ email });
+      if (!foundUser) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid credentials.' }] });
+      }
+
+      //after the user is registered, return a token for authentication
+      let payload = {
+        user: {
+          id: newUser.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.JWTSECRET,
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      res.status(500).json('Server error.');
+    }
+  }
+);
 
 module.exports = authRouter;
