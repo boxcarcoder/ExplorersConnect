063c8a1f49e6abb4d14cf368f2f02066ca6eef97
Brendan Cheng <brendancheng333@gmail.com>
2019-11-24 17:23:02 -0800

    added API routes to fetch all posts, posts by id, and delete by id

--- controllers/posts.js
+++ controllers/posts.js
@@ -46,4 +46,64 @@ postsRouter.post(
   }
 );
 
+// @route   GET api/posts
+// @desc    Fetch all posts
+// @access  Public so site visitors can see posts for more exposure
+postsRouter.get('/', async (req, res) => {
+  try {
+    let posts = await Post.find().sort({ date: -1 }); //sort posts by most recent
+
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json('Server Error.');
+  }
+});
+
+// @route   GET api/posts/:postID
+// @desc    Fetch post by ID
+// @access  Public so site visitors can see posts for more exposure
+postsRouter.get('/:postID', async (req, res) => {
+  try {
+    let post = await Post.findById(req.params.postID); //sort posts by most recent
+
+    if (!post) return res.status(400).json('This post could not be found.');
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      //a properly formatted ID was not found in the request
+      return res.status(400).status('This post could not be found.');
+    }
+    res.status(500).json('Server Error.');
+  }
+});
+
+// @route   DELETE api/posts/:postID
+// @desc    Delete post by ID
+// @access  Private since only a logged in user can delete their post
+postsRouter.delete('/:postID', auth, async (req, res) => {
+  try {
+    let post = await Post.findById(req.params.postID);
+
+    if (!post) return res.status(400).json('This post could not be found.');
+
+    // check if user is the logged in user
+    if (req.user.id !== post.user.toString()) {
+      return res.status(401).json('User is not authorized.');
+    }
+
+    await post.remove();
+
+    res.json('Post removed.');
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(400).status('This post could not be found.');
+    }
+    res.status(500).json('Server Error.');
+  }
+});
+
 module.exports = postsRouter;
