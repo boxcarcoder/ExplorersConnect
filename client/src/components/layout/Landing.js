a98b713472cef571d11ecb00b94ee353d23035ae
Brendan Cheng <brendancheng333@gmail.com>
2020-01-08 18:46:48 -0800

    fixed inline styles in JSX for photo credit

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -20,19 +20,39 @@ const Landing = () => {
           </div>
         </div>
       </div>
-      {/* <a
-        style={{background-color:'black';color:'white';text-decoration:'none';padding:'4px 6px';font-family:'-apple-system , BlinkMacSystemFont, \"San Francisco\", \"Helvetica Neue\", Helvetica, Ubuntu, Roboto, Noto, \"Segoe UI\", Arial, sans-serif ';font-size:'8px';font-weight:'bold';line-height:'1';display:'inline-block';border-radius:'3px'}}
+      <a
+        style={{
+          backgroundColor: 'black',
+          color: 'white',
+          textDecoration: 'none',
+          padding: '4px 6px',
+          fontFamily:
+            '-apple-system , BlinkMacSystemFont, "San Francisco", "Helvetica Neue", Helvetica, Ubuntu, Roboto, Noto, "Segoe UI", Arial, sans-serif ',
+          fontSize: '8px',
+          fontWeight: 'bold',
+          lineHeight: '1',
+          display: 'inline-block',
+          borderRadius: '3px'
+        }}
         href='https://unsplash.com/@wanderingteddybear?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge'
         target='_blank'
         rel='noopener noreferrer'
         title='High-resolution photos from Ted Bryan Yu on Unsplash'
         className='credit-box-abs'
       >
-        <span style='display:inline-block;padding:2px 3px;font-size:11px'>
+        <span
+          style={{
+            display: 'inline-block',
+            padding: '2px 3px',
+            fontSize: '11px'
+          }}
+        >
           <i className='fas fa-camera-retro'></i>
         </span>
-        <span style='display:inline-block;padding:2px 3px'>Ted Bryan Yu</span>
-      </a> */}
+        <span style={{ display: 'inline-block', padding: '2px 3px' }}>
+          Ted Bryan Yu
+        </span>
+      </a>
     </section>
   );
 };
