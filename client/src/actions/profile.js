ddee39d8d04391b4e1790b02a5758ff89a528ba4
Brendan Cheng <brendancheng333@gmail.com>
2020-01-20 19:35:11 -0800

    fixed typos with profile action and reducers.

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -2,26 +2,25 @@ import { GET_PROFILE, PROFILE_ERROR } from './types';
 import axios from 'axios';
 import { setAlert } from './alert';
 
-
 // Get the current user's profile.
-// 
+//
 export const getCurrentProfile = () => async dispatch => {
-    try {
-        // our backend returns the profile of the logged in user
-        // as the res when fetching from /api/profile/me
-        const res = await axios.get('/api/profile/me');
+  try {
+    // our backend returns the profile of the logged in user
+    // as the res when fetching from /api/profile/me
+    const res = await axios.get('/api/profiles/me');
 
-        // dispatch profile data to reducer to save the profile data into the profile redux state
-        dispatch({
-            type: GET_PROFILE,
-            payload: res.data
-        })
-
-    } catch(err) {
-        // dispatch error message and HTTP error status to the profile redux state
-        dispatch({
-            type: PROFILE_ERROR,
-            payload: { msg: err.response.statusText, status: err.response.status}
-        })
-    }
+    // dispatch profile data to reducer to save the profile data into the profile redux state
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+  } catch (err) {
+    console.log('error:', err);
+    // dispatch error message and HTTP error status to the profile redux state
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
 };
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -3,30 +3,30 @@ import { GET_PROFILE, PROFILE_ERROR } from '../actions/types';
 // profile: When the user logs in, or visits another user's profile page,
 // an HTTP request will be made that returns the profile's data
 // profiles: For the list of all profiles for the explorers page.
-initialState = {
-    profile = null, 
-    profiles = [],
-    loading = true,
-    error: {}
-}
+const initialState = {
+  profile: null,
+  profiles: [],
+  loading: true,
+  error: {}
+};
 
-export default function(state=initialState, action) {
-    const { type, payload } = action;
+export default function(state = initialState, action) {
+  const { type, payload } = action;
 
-    switch(type) {
-        case GET_PROFILE:
-            return {
-                ...state,
-                profile: payload, //the payload is the profile data returned from the GET request to the backend route /api/profile/me
-                loading: false
-            }
-        case PROFILE_ERROR:
-            return {
-                ...state,
-                error: payload,
-                loading: false
-            }
-        default:
-            return state;
-    }
+  switch (type) {
+    case GET_PROFILE:
+      return {
+        ...state,
+        profile: payload, //the payload is the profile data returned from the GET request to the backend route /api/profile/me
+        loading: false
+      };
+    case PROFILE_ERROR:
+      return {
+        ...state,
+        error: payload,
+        loading: false
+      };
+    default:
+      return state;
+  }
 }
