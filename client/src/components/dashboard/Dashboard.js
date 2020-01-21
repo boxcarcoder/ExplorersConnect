973d4319e97a0d4d62f602ceb65b38150b7142d8
Brendan Cheng <brendancheng333@gmail.com>
2020-01-20 19:09:19 -0800

    created profile action and reducer for profile state for dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,12 +1,20 @@
 // Fetch all relevant data using actions, bring them in from the redux state,
 // and pass them down to components, such as Destinations and Gears 
 
-import React from 'react';
+import React, { useEffect } from 'react';
 
 //redux
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profile';
 
-const Dashboard = () => {
+
+
+const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+    useEffect(() => {
+        getCurrentProfile();
+    }, []); //on the dashboard's first load, retrieve the logged in user's profile and save it into the profile redux state
+
     return (
         <div>
             Dashboard
@@ -14,6 +22,15 @@ const Dashboard = () => {
     );
 }
 
-Dashboard.propTypes = {};
+Dashboard.propTypes = {
+    getCurrentProfile: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    profile: PropTypes.object.isRequired    
+};
 
-export default Dashboard;
+const mapStateToProps = state => ({
+    auth: state.auth,
+    profile: state.profile
+});
+
+export default connect(mapStateToProps, {getCurrentProfile})(Dashboard);
