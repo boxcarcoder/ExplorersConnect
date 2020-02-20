9a6e1999a63a2908e798bee986831cc2c130dc93
Brendan Cheng <brendancheng333@gmail.com>
2020-02-19 20:39:53 -0800

    fixed img pathing in profiles component.

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -1,63 +1,63 @@
 import React, { Fragment } from 'react';
+import hikerPic from '../../img/luke-pamer-KBpnPk44tOA-unsplash.jpg';
+import snowboardPic from '../../img/joshua-reddekopp-9BpTTLoTv9w-unsplash.jpg';
 
 const Profiles = () => {
-    return (
-        <Fragment>
-            <h1 class="large text-primary">
-                Explorers
-            </h1>
-            <p class="lead">
-                <i class="fab fa-connectdevelop"></i> Browse and connect with explorers
-            </p>
-            <div class="profiles">
-                <div class="profile bg-light">
-                    <img
-                        class="round-img"
-                        src="img/luke-pamer-KBpnPk44tOA-unsplash.jpg"
-                        alt="hiker"
-                    />
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Explorers</h1>
+      <p className='lead'>
+        <i className='fab fa-connectdevelop'></i> Browse and connect with
+        explorers
+      </p>
+      <div className='profiles'>
+        <div className='profile bg-light'>
+          <img className='round-img' src={hikerPic} alt='hiker' />
 
-                    <div>
-                        <h2>John Doe</h2>
-                        <p>Seattle, WA</p>
-                        <a href="profile.html" class="btn btn-primary">
-                        View Profile
-                        </a>
-                    </div>
+          <div>
+            <h2>John Doe</h2>
+            <p>Seattle, WA</p>
+            <a href='profile.html' className='btn btn-primary'>
+              View Profile
+            </a>
+          </div>
 
-                    <ul>
-                        <li class="text-primary"><i class="fas fa-check"></i> Camping</li>
-                        <li class="text-primary"><i class="fas fa-check"></i> Hiking</li>
-                    </ul>
-                    
-                    </div>
+          <ul>
+            <li className='text-primary'>
+              <i className='fas fa-check'></i> Camping
+            </li>
+            <li className='text-primary'>
+              <i className='fas fa-check'></i> Hiking
+            </li>
+          </ul>
+        </div>
 
-                    <div class="profile bg-light">
-                    <img
-                        class="round-img"
-                        src="img/joshua-reddekopp-9BpTTLoTv9w-unsplash.jpg"
-                        alt=""
-                    />
+        <div className='profile bg-light'>
+          <img className='round-img' src={snowboardPic} alt='snowboarder' />
 
-                    <div>
-                        <h2>Jane Doe</h2>
-                        <p>Denver, CO</p>
-                        <a href="profile.html" class="btn btn-primary">
-                        View Profile
-                        </a>
-                    </div>
+          <div>
+            <h2>Jane Doe</h2>
+            <p>Denver, CO</p>
+            <a href='profile.html' className='btn btn-primary'>
+              View Profile
+            </a>
+          </div>
 
-                    <ul>
-                        <li class="text-primary"><i class="fas fa-check"></i> Hiking</li>
-                        <li class="text-primary"><i class="fas fa-check"></i> Kayaking</li>
-                        <li class="text-primary">
-                        <i class="fas fa-check"></i> Snowboarding
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </Fragment>
-    );
-}
+          <ul>
+            <li className='text-primary'>
+              <i className='fas fa-check'></i> Hiking
+            </li>
+            <li className='text-primary'>
+              <i className='fas fa-check'></i> Kayaking
+            </li>
+            <li className='text-primary'>
+              <i className='fas fa-check'></i> Snowboarding
+            </li>
+          </ul>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
 
 export default Profiles;
