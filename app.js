7b87bddd9408de933b0116ea22e053be739a7662
Brendan Cheng <brendancheng333@gmail.com>
2020-03-19 12:17:02 -0700

    prepare server for deployment.

--- app.js
+++ app.js
@@ -1,25 +1,15 @@
 const config = require('./utils/config');
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
 const usersRouter = require('./controllers/users');
 const authRouter = require('./controllers/auth');
 const profilesRouter = require('./controllers/profiles');
 const postsRouter = require('./controllers/posts');
 const bodyParser = require('body-parser');
+const path = require('path');
 
-// mongoose
-//   .connect(config.MONGODB_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-//   })
-//   .then(result => {
-//     console.log('database connected successfully.');
-//   })
-//   .catch(err => {
-//     console.error('error connecting to db:', err.message);
-//     process.exit(1);
-//   });
+// initialize app with Express
+const app = express();
 
 // connect to MongoDB
 const connectDB = async () => {
@@ -45,5 +35,18 @@ app.use('/api/users', usersRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/profiles', profilesRouter);
 app.use('/api/posts', postsRouter);
+
+// Server static assets in production.
+// process.env.NODE_ENV dictates whether an environment is a production or development production.
+// Setting NODE_ENV is done through CLI; not recommended to be set within a node application.
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder using express
+  app.use(express.static('client/build'));
+
+  // Serve the static HTML file
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 module.exports = app;
