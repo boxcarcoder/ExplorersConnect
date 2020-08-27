bb92003031dd2dd041e42cd29592781988a83cb9
Brendan Cheng <brendancheng333@gmail.com>
2020-08-27 12:06:55 -0700

    created test for post action: likePost()

--- client/src/__mocks__/axios.js
+++ client/src/__mocks__/axios.js
@@ -1,4 +1,5 @@
 export default {
   get: jest.fn(() => Promise.resolve({ data: 'get test data' })),
   post: jest.fn(() => Promise.resolve({ data: 'POST test data' })),
+  put: jest.fn(() => Promise.resolve({ data: 'put test data' })),
 };
