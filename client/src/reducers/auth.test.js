8e974a834e790419575f2ac0fa6b7e4983304c2d
Brendan Cheng <brendancheng333@gmail.com>
2020-09-01 13:56:49 -0700

    completed tests for Auth Reducer.

--- client/src/reducers/auth.test.js
+++ client/src/reducers/auth.test.js
@@ -7,11 +7,10 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   LOGOUT,
-  ACCOUNT_DELETED,
 } from '../actions/types';
 
 describe('Auth Reducer.', () => {
-  test('The initial state is returned.', async () => {
+  test('The initial state is returned.', () => {
     // Create a test action to send to the reducer.
     let testAction = {};
 
@@ -28,8 +27,8 @@ describe('Auth Reducer.', () => {
     expect(reducer).toEqual(initialState);
   });
 
-  describe('Handle receiving the REGISTER_SUCCESS action.', () => {
-    test('Returns the correct state.', async () => {
+  describe('Receive the the REGISTER_SUCCESS action.', () => {
+    test('Returns the correct state.', () => {
       // Create a test action to send to the reducer.
       let testAction = {
         type: REGISTER_SUCCESS,
@@ -52,11 +51,134 @@ describe('Auth Reducer.', () => {
     });
   });
 
-  describe('Handle receiving the REGISTER_FAIL action.', () => {
-    test('Returns the correct state.', async () => {
+  describe('Receive the the REGISTER_FAIL action.', () => {
+    test('Returns the correct state.', () => {
       // Create a test action to send to the reducer
       let testAction = {
         type: REGISTER_FAIL,
+      };
+
+      // Send the test action to the reducer.
+      const reducer = auth(undefined, testAction);
+
+      // Execute the test.
+      const expectedState = {
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+        loggedInUser: null,
+      };
+      expect(reducer).toEqual(expectedState);
+    });
+  });
+
+  describe('Receive the the USER_LOADED action.', () => {
+    test('Returns the correct state.', () => {
+      // Create a test action.
+      let testAction = {
+        type: USER_LOADED,
+        payload: {
+          user: {
+            name: 'test name',
+            email: 'test@test.com',
+            avatar: '',
+            date: '',
+          },
+        },
+      };
+
+      // Send the test action to the reducer.
+      const reducer = auth(undefined, testAction);
+
+      // Execute the test.
+      const expectedState = {
+        token: localStorage.getItem('token'),
+        isAuthenticated: true,
+        loading: false,
+        loggedInUser: {
+          user: {
+            name: 'test name',
+            email: 'test@test.com',
+            avatar: '',
+            date: '',
+          },
+        },
+      };
+      expect(reducer).toEqual(expectedState);
+    });
+  });
+
+  describe('Receive the AUTH_ERROR action.', () => {
+    test('Returns the correct state.', () => {
+      // Create a test action.
+      let testAction = {
+        type: AUTH_ERROR,
+      };
+
+      // Send the test action to the reducer.
+      const reducer = auth(undefined, testAction);
+
+      // Execute the test.
+      const expectedState = {
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+        loggedInUser: null,
+      };
+      expect(reducer).toEqual(expectedState);
+    });
+  });
+
+  describe('Receive the LOGIN_SUCCESS action.', () => {
+    test('Returns the correct state.', () => {
+      // Create a test action.
+      let testAction = {
+        type: LOGIN_SUCCESS,
+        payload: {
+          token: 'test token',
+        },
+      };
+
+      // Send the test action to the reducer.
+      const reducer = auth(undefined, testAction);
+
+      // Execute the test.
+      const expectedState = {
+        token: localStorage.getItem('token'),
+        isAuthenticated: true,
+        loading: false,
+        loggedInUser: null,
+      };
+      expect(reducer).toEqual(expectedState);
+    });
+  });
+
+  describe('Receive the LOGIN_FAIL action.', () => {
+    test('Returns the correct state.', () => {
+      // Create a test action.
+      let testAction = {
+        type: LOGIN_FAIL,
+      };
+
+      // Send the test action to the reducer.
+      const reducer = auth(undefined, testAction);
+
+      // Execute the test.
+      const expectedState = {
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+        loggedInUser: null,
+      };
+      expect(reducer).toEqual(expectedState);
+    });
+  });
+
+  describe('Receive the LOGOUT action.', () => {
+    test('Returns the correct state.', () => {
+      // Create a test action.
+      let testAction = {
+        type: LOGOUT,
       };
 
       // Send the test action to the reducer.
