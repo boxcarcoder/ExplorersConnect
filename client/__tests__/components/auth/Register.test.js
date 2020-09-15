2c713b4dae869e12003ef603e0e5a2cb88288441
Brendan Cheng <brendancheng333@gmail.com>
2020-09-15 12:13:56 -0700

    create a help branch to host my attempts at testing a component

--- client/__tests__/components/auth/Register.test.js
+++ client/__tests__/components/auth/Register.test.js
@@ -1,26 +1,91 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
-import configureMockStore from 'redux-mock-store'; // Smart components
-import { Provider } from 'react-redux'; // needed to render a store that connects to a redux store
+import configureMockStore from 'redux-mock-store';
+import { Provider } from 'react-redux'; // needed to render a component that connects to a redux store
 
 // Component to be tested
 import Register from '../../../src/components/auth/Register';
+// Props for the component
+import { setAlert } from '../../../src/actions/alert';
+import { register } from '../../../src/actions/auth';
 
 const mockStore = configureMockStore();
-const store = mockStore({});
+let initialState = {
+  token: localStorage.getItem('token'),
+  isAuthenticated: false,
+  loading: false,
+  loggedInUser: null,
+};
+const store = mockStore(initialState);
 
-describe('<Register />', () => {
-  describe('render()', () => {
-    test('renders the component', () => {
-      const wrapper = shallow(
-        <Provider store={store}>
-          <Register />
-        </Provider>
-      );
-      const component = wrapper.dive();
+describe('<Register /> component.', () => {
+  let props;
 
-      expect(toJson(component)).toMatchSnapshot();
-    });
+  beforeEach(() => {
+    props = {
+      setAlert: setAlert,
+      register2: register,
+      authState: {
+        token: localStorage.getItem('token'),
+        isAuthenticated: false,
+        loading: false,
+        loggedInUser: null,
+      },
+    };
+  });
+
+  // Unit tests
+  test('Find the container rendered in the Register component.', () => {
+    // Attempt #5
+    const wrapper = mount(
+      <Provider store={store}>
+        <Register {...props} />
+      </Provider>
+    );
+    expect(wrapper.length).toBe(1);
+    wrapper.debug();
+    expect(wrapper.find('.container')).to.have.lengthOf(1);
+
+    // Attempt #4
+    const wrapper = shallow(
+      <Provider store={store}>
+        <Register {...props} />
+      </Provider>
+    );
+    expect(wrapper.length).toBe(1);
+    wrapper.debug();
+    expect(wrapper.find('.container')).to.have.lengthOf(1);
+
+    // Attempt #3
+    const wrapper = shallow(
+      <Provider store={store}>
+        <Register {...props} />
+      </Provider>
+    ).dive();
+    expect(wrapper.length).toBe(1);
+    wrapper.debug();
+    expect(wrapper.hasClass('container')).toEqual(true);
+
+    // Attempt #2
+    const wrapper = shallow(
+      <Provider store={store}>
+        <Register {...props} />
+      </Provider>
+    );
+    expect(wrapper.length).toBe(1);
+    wrapper.debug();
+    expect(wrapper.hasClass('container')).toEqual(true);
+
+    // Attempt #1
+    const wrapper = shallow(
+      <Provider store={store}>
+        <Register {...props} />
+      </Provider>
+    );
+    expect(wrapper.length).toBe(1);
+    wrapper.debug();
+    const container = wrapper.find('.container');
+    expect(container.length).toBe(1);
   });
 });
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -8,35 +8,36 @@ import PropTypes from 'prop-types';
 import { register } from '../../actions/auth';
 
 const Register = ({ setAlert, register, authState: { isAuthenticated } }) => {
+  console.log('begin rendering Register component.');
   //each input requires a state and onChange handler
   const [formData, setFormData] = useState({
     name: '',
     email: '',
     password: '',
-    confirmPassword: ''
+    confirmPassword: '',
   });
 
   //destructuring: name === formData.name
   const { name, email, password, confirmPassword } = formData;
 
   // spread operator: ...formData === copy of formData based on each attribute
-  const handleNameChange = e => {
+  const handleNameChange = (e) => {
     setFormData({ ...formData, name: e.target.value });
   };
 
-  const handleEmailChange = e => {
+  const handleEmailChange = (e) => {
     setFormData({ ...formData, email: e.target.value });
   };
 
-  const handlePasswordChange = e => {
+  const handlePasswordChange = (e) => {
     setFormData({ ...formData, password: e.target.value });
   };
 
-  const handleConfirmPasswordChange = e => {
+  const handleConfirmPasswordChange = (e) => {
     setFormData({ ...formData, confirmPassword: e.target.value });
   };
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     if (password !== confirmPassword) {
       // after connecting the setAlert action, we can use it by passing a prop to the component.
@@ -48,6 +49,7 @@ const Register = ({ setAlert, register, authState: { isAuthenticated } }) => {
   };
 
   // once the user is registered, redirect to the dashboard
+  console.log('isAuthenticated: ', isAuthenticated);
   if (isAuthenticated) {
     return <Redirect to='/dashboard' />;
   }
@@ -64,7 +66,7 @@ const Register = ({ setAlert, register, authState: { isAuthenticated } }) => {
             type='text'
             placeholder='Name'
             value={name}
-            onChange={e => handleNameChange(e)}
+            onChange={(e) => handleNameChange(e)}
             required
           />
         </div>
@@ -73,7 +75,7 @@ const Register = ({ setAlert, register, authState: { isAuthenticated } }) => {
             type='email'
             placeholder='Email'
             value={email}
-            onChange={e => handleEmailChange(e)}
+            onChange={(e) => handleEmailChange(e)}
             required
           />
         </div>
@@ -82,7 +84,7 @@ const Register = ({ setAlert, register, authState: { isAuthenticated } }) => {
             type='password'
             placeholder='Password'
             value={password}
-            onChange={e => handlePasswordChange(e)}
+            onChange={(e) => handlePasswordChange(e)}
             required
           />
         </div>
@@ -91,7 +93,7 @@ const Register = ({ setAlert, register, authState: { isAuthenticated } }) => {
             type='password'
             placeholder='Confirm Password'
             value={confirmPassword}
-            onChange={e => handleConfirmPasswordChange(e)}
+            onChange={(e) => handleConfirmPasswordChange(e)}
           />
         </div>
         <input type='submit' value='Register' className='btn btn-primary' />
@@ -107,12 +109,12 @@ const Register = ({ setAlert, register, authState: { isAuthenticated } }) => {
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
   register: PropTypes.func.isRequired,
-  authState: PropTypes.object.isRequired
+  authState: PropTypes.object.isRequired,
 };
 
 //mapping the auth redux state to a usable prop to check for authentication
-const mapStateToProps = state => ({
-  authState: state.auth //propName: state
+const mapStateToProps = (state) => ({
+  authState: state.auth,
 });
 
 // in order to use connect() to use actions.
